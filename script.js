9dfbe15b710b96f233559ab94e55527a81944a48
Berkay Yetgin <85676216+Yakrel@users.noreply.github.com>
2023-01-19 17:44:19 +0300

    add all choices and fix few things

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 const choices = ["rock", "paper", "scissor"];
-function getComputerChoice() {
-  return choices[Math.floor(Math.random() * 3) + 0];
+let playerWins = 0;
+let computerWins = 0;
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+    console.log("Player Score = " + playerWins);
+    console.log("Computer Score = " + computerWins);
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
+  function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3) + 0];
+  }
   computerSelection = getComputerChoice();
   console.log("Computer Selected " + computerSelection);
   playerSelection = prompt("Your Choice");
-  playerSelection = playerSelection.toLowerCase()
+  playerSelection = playerSelection.toLowerCase();
   console.log("Player Selected " + playerSelection);
   if (playerSelection == "rock" && computerSelection == "paper") {
+    computerWins++;
     return console.log("Computer Win");
   } else if (playerSelection == "paper" && computerSelection == "rock") {
+    playerWins++;
     return console.log("player win");
   } else if (playerSelection == "rock" && computerSelection == "scissor") {
+    playerWins++;
     return console.log("player win");
   } else if (playerSelection == "scissor" && computerSelection == "rock") {
+    computerWins++;
     return console.log("Computer win");
   } else if (playerSelection == "paper" && computerSelection == "scissor") {
+    computerWins++;
     return console.log("Computer win");
-  } else if (playerSelection == "scissor" && computerSelection == "paper")
+  } else if (playerSelection == "scissor" && computerSelection == "paper") {
+    playerWins++;
     return console.log("player win");
-  else console.log("No Winner");
+  } else console.log("No Winner");
 }
