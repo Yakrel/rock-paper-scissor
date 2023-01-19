5377ccb4d97b5a9862da21f3ec4e4f64730e255c
Berkay Yetgin <85676216+Yakrel@users.noreply.github.com>
2023-01-19 10:08:50 +0300

    autor reset

---
+++ script.js
@@ -0,0 +1,24 @@
+const choices = ["rock", "paper", "scissor"];
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * 3) + 0];
+}
+
+function playRound(playerSelection, computerSelection) {
+  computerSelection = getComputerChoice();
+  console.log("Computer Selected " + computerSelection);
+  playerSelection = prompt("Your Choice");
+  console.log("Player Selected " + playerSelection);
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    return console.log("Computer Win");
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return console.log("player win");
+  } else if (playerSelection == "rock" && computerSelection == "scissor") {
+    return console.log("player win");
+  } else if (playerSelection == "scissor" && computerSelection == "rock") {
+    return console.log("Computer win");
+  } else if (playerSelection == "paper" && computerSelection == "scissor") {
+    return console.log("Computer win");
+  } else if (playerSelection == "scissor" && computerSelection == "paper")
+    return console.log("player win");
+  else console.log("No Winner");
+}
